2294b12519b71b57bc89998f5b0eae6943511a6f
josepallas <josepallaspn@gmail.com>
2020-09-26 21:40:36 +0200

    Añadido proyecto web

---
+++ src/FilmLibrary.Web/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using FilmLibrary.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilmLibrary.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private FilmService filmService;
+        public HomeController()
+        {
+            filmService = new FilmService();
+        }
+        public ActionResult Index()
+        {
+            var list = filmService.GetFilms();
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ src/FilmLibrary.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FilmLibrary.Web.MvcApplication" Language="C#" %>
